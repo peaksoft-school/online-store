3f901d6cd130234b8ee2549501d4f684daa0f616
Kanat <ergeshmametuulukanat@gmail.com>
2021-06-15 21:56:03 +0600

    settings

---
+++ online-store/src/main/java/kg/online_store/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package kg.online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}
