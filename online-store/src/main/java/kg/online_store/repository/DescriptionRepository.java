9116eed27bfd2794125fd1eadfdcf7d1db2dd701
Kanat <46622639+kanatsubanov321@users.noreply.github.com>
2021-07-13 20:56:37 +0600

    Revert "Feature/controller"

--- online-store/src/main/java/kg/online_store/repository/DescriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package kg.online_store.repository;
-
-import kg.online_store.model.Description;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DescriptionRepository extends JpaRepository<Description, Long> {
-}
--- online-store/src/main/java/kg/online_store/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package kg.online_store.service;
-
-import kg.online_store.model.Role;
-
-public interface RoleService {
-    void save(Role role);
-    void deleteById(long id);
-}
