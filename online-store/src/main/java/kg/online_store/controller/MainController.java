6fa96e68c304c8c868ed4edb8adaa6f76118de6b
Kanat Subanov <subanovkanat321@gmail.com>
2021-07-27 19:15:23 +0300

    added MainController.java

---
+++ online-store/src/main/java/kg/online_store/controller/MainController.java
@@ -0,0 +1,14 @@
+package kg.online_store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main_page")
+public class MainController {
+    @GetMapping
+    public String loginPage() {
+        return "main-page";
+    }
+}
