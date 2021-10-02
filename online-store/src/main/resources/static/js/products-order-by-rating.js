da58b67adbec7a0ef858db288d0536d87d88f6ad
Atabek <ataissakunov@gmail.com>
2021-10-02 16:36:14 +0600

    Commit for checkpoint

--- online-store/src/main/resources/static/js/products-order-by-rating.js
+++ online-store/src/main/resources/static/js/products-order-by-rating.js
@@ -47,25 +47,3 @@ function renderProduct(data) {
         productList.append(div);
     }
 }
-//     result.forEach(product=>{
-//         html_actual += `
-//                   <div class="col-md-4" >
-//                     <br>
-//                     <a href="#">
-//                     <div class="container">
-//                         <img src="${product.productPicture}" alt=""
-//                              width="220" height="200">
-//                     </div>
-//                     <div class="text-center-left">
-//                         <p>${product.name}</p>
-//                         <p1 style="color: black">price:${product.productPrice}</p1>
-//                         <br>
-//                     </div>
-//                     </a>
-//                     <br>
-//                 </div>
-// `;
-//     })
-//     console.log(html_actual)
-//     document.getElementById('productList').innerHTML = html_actual;
-// })
